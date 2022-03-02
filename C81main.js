f9988bd8b6026f482500b17819fd63cb60569637
AsmeexD <90191995+AsmeexD@users.noreply.github.com>
2022-03-02 16:59:24 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,28 @@
+canvas= document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+
+color= "red";
+
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=4;
+ctx.arc(200, 200, 50 , 0, 2*Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown" , mymouse_down);
+
+function mymouse_down(e){
+    mouse_x= e.clientX-canvas.offsetLeft;
+    mouse_y= e.clientY-canvas.offsetTop;
+
+    circle(mouse_x , mouse_y)
+
+}
+
+function circle(mouse_x, mouse_y){
+    ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=4;
+ctx.arc(mouse_x, mouse_y, 50 , 0, 2*Math.PI);
+ctx.stroke();
+}
